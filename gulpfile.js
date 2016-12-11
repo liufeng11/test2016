8bdb445847e34948de2b43234fa8ac0d36e734c9
liufeng11 <2410860150@qq.com>
2016-12-11 19:59:24 +0800

    revised

--- gulpfile.js
+++ gulpfile.js
@@ -1,209 +1,82 @@
+
+var del=require('del');
 var gulp=require('gulp');
 var uglify=require('gulp-uglify');
-var concat=require('gulp-concat');
-var through = require("through2");
-var jshint = require('gulp-jshint');
-var imagemin = require('gulp-imagemin');//图片压缩
-var pngcrush = require('imagemin-pngcrush');
-var pngquant = require('imagemin-pngquant');//图片深入压缩
-var  imageminOptipng = require('imagemin-optipng');
-var  imageminSvgo = require('imagemin-svgo');
-var  imageminGifsicle = require('imagemin-gifsicle');
-var  imageminJpegtran = require('imagemin-jpegtran');
-var plumber = require('gulp-plumber');//检测错误
-var gutil = require('gulp-util');//如果有自定义方法，会用到
- var cache = require('gulp-cache');//图片压缩缓存
-var clean = require('gulp-clean');//清空文件夹
-var conCat = require('gulp-concat');//文件合并
-var connect = require("gulp-connect");
-var htmlmin = require('gulp-htmlmin');//html压缩
-var  processhtml = require('gulp-processhtml');
- var   Replace = require('gulp-replace');
-var date = new Date().getTime();
-var livereload = require('gulp-livereload');//服务器<-->文件自动更新
-var webserver = require('gulp-webserver'); // 本地服务器
-var domSrc = require('gulp-dom-src');
-var    cheerio = require('gulp-cheerio');
+var imagemin=require('gulp-imagemin');
+var mincss=require('gulp-clean-css');//压缩css
+var inline=require('gulp-inline-source'); //资源内联 （主要是js，css，图片）
+var include=require('gulp-include'); //资源内联（主要是html片段）
+var sequence=require('gulp-sequence');
+var useref=require('gulp-useref'); //合并文件
+var gulpif=require('gulp-if');
+var print=require('gulp-print'); //打印命中的文件
+var connect=require('gulp-connect'); //本地服务器
 
-function errrHandler( e ){
-    // 控制台发声,错误时beep一下
-    gutil.beep();
-    gutil.log(e);
-    this.emit('end');
-}
 
-function minifyAndComboCSS(name, encode, files){
-  var fs = require("fs");
-  var CleanCSS = require("clean-css");
-  var content = "";
-
-  files.forEach(function(css){
-    var contents = fs.readFileSync(css, encode);
-    var minified = new CleanCSS().minify(contents).styles; 
-    content += minified;
-  });
-
-  if(content){
-    var combo = "../**/*.css";
-  }
-  fs.writeFileSync(combo, content);
-
-  gulp.src(combo)
-      .pipe(gulp.dest("./dist/static/css"));
-}
-
-gulp.task("lint", function() {
-  return gulp.src("./public/xd.js")
-    .pipe(jshint())
-    .pipe(jshint.reporter('default', { verbose: true }));
-});
-
- 
-
-function minifyAndComboJS(name, encode, files){
-  var fs = require("fs");
-  var UglifyJS = require("uglify-js");
-  var content = "";
-
-  files.forEach(function(js){
-    var minified = UglifyJS.minify(js).code;
-    content += minified;
-  });
-
-  if(content){
-    var combo = "./public/js/*.js";
-  }
-  fs.writeFileSync(combo, content);
-
-  gulp.src(combo)
-      .pipe(gulp.dest("./dist/static/js"));
-}
-
-gulp.task("clean",function(){
-    return gulp.src('dist/concatjs/**',{read:false})
-        .pipe(clean());
+//清理构建目录
+gulp.task('clean',function (cb) {
+    del(['dist']).then(function () {
+        cb();
+    })
 });
 
 
 
-gulp.task('build-index', function () {
-    var options = {
-        removeComments: true,//清除HTML注释
-        collapseWhitespace: true,//压缩HTML
-        collapseBooleanAttributes: false,//省略布尔属性的值 <input checked="true"/> ==> <input />
-        removeEmptyAttributes: false,//删除所有空格作属性值 <input id="" /> ==> <input />
-        removeScriptTypeAttributes: true,//删除<script>的type="text/javascript"
-        removeStyleLinkTypeAttributes: true,//删除<style>和<link>的type="text/css"
-        minifyJS: true,//压缩页面JS
-        minifyCSS: true//压缩页面CSS
-    };
-    gulp.src('./public/*.html')       
-        .pipe(plumber({errorHandler:errrHandler}))     
-        .pipe(Replace(/_VERSION_/gi, date))
-        .pipe(processhtml())
-        .pipe(htmlmin(options))
-        .pipe(gulp.dest('dist/html'));
+gulp.task('mincss',function () {
+    return gulp.src('./public/*.css')
+        .pipe(mincss())
+        .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('minimg', function () {
+    gulp.src('./public/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'))
 });
 
 
-
-
-
-
-
-
-gulp.task("build-js-lib", function(){
-  gulp.src("./public/**/*.js")
-      .pipe(through.obj(function(file, encode, cb) {
-        var UglifyJS = require("uglify-js");
-
-        var contents = file.contents.toString(encode);
-        var minified = UglifyJS.minify(contents, 
-          {fromString:true}).code;
-
-        file.contents = new Buffer(minified, encode);
-        cb(null, file, encode);
-      }))
-      .pipe(gulp.dest("./dist/static/js/lib"));
-});
-
-gulp.task("build-common-css", function(){
-  gulp.src("./public/*.css")
-      .pipe(through.obj(function(file, encode, cb) {
-        var CleanCSS = require("clean-css");
-
-        var contents = file.contents.toString(encode);
-        var minified = new CleanCSS().minify(contents).styles;  
-
-        file.contents = new Buffer(minified, encode);
-        cb(null, file, encode);
-      }))
-      .pipe(gulp.dest("./dist/static/css/common"));
-});
-
-
-gulp.task("img", function () {
-    gulp.src('./public/img/*.{png,jpg,gif,ico}')
-        .pipe(plumber({errorHandler:errrHandler}))
-        .pipe(cache(imagemin({     
-            progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片          
-            svgoPlugins: [{removeViewBox: false}],//不要移除svg的viewbox属性
-            use: [pngquant(),imageminJpegtran({progressive: true})
-            , imageminGifsicle({interlaced: true}),imageminOptipng({optimizationLevel:3}), imageminSvgo()] //使用pngquant深度压缩png图片的imagemin插件          
-        })))
-        .pipe(gulp.dest('dist/images'));
-});
-
-
-gulp.task('jsmin', function () {
-    gulp.src(['./public/js/*.js'])
-        .pipe(conCat('./public/js/xd.js'))
-        .pipe(plumber({errorHandler:errrHandler}))
-        .pipe(uglify({
-            mangle: {except: ['require' ,'exports' ,'module' ,'$']},//类型：Boolean 默认：true 是否修改变量名
-            compress: true,//类型：Boolean 默认：true 是否完全压缩
-            preserveComments: 'false' //保留所有注释
-        }))
-        .pipe(gulp.dest('dist/concatjs'));
-}); 
-
-
-
-gulp.task('watch', function(){
-    //gulp.run( "connect","build-index","build-js-lib", "build-common-css","lint","img","jsmin","clean");
- // 监听html文件变化
-    //gulp.watch('./public/*.html', function(){
-      //  gulp.run("build-index");
-   // });
-      gulp.watch('./public/*.html', ["html"]);
-    // Watch .css files
-    gulp.watch('./public/*.css', ["build-common-css"]);
-
-    // Watch .js files
-    gulp.watch('./public/js/*.js', ['lint', "build-js-lib",'jsmin']);
-
-    // Watch image files
-    gulp.watch('./public/img/*', ['img']);
-});
-
- gulp.task('connect',function(){
-    connect.server({
-        root:'./',  
-        ip:'192.168.31.110',
-        livereload:true
-    });
+gulp.task('minjs',function () {
+    return gulp.src('./public/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'))
 });
 
 
 gulp.task('html', function () {
-  gulp.src('./public/*.html')
-    .pipe(connect.reload());
+    return gulp.src('./public/*.html')
+        .pipe(inline())//把js内联到html中
+        .pipe(include())//把html片段内联到html主文件中
+        .pipe(useref())//根据标记的块  合并js或者css
+        .pipe(gulpif('*.js',uglify()))
+        .pipe(gulpif('*.css',mincss()))
+        .pipe(connect.reload()) //重新构建后自动刷新页面
+        .pipe(gulp.dest('dist'));
+});
+
+
+//本地服务器  支持自动刷新页面
+gulp.task('connect', function() {
+    connect.server({
+        root: './dist', //本地服务器的根目录路径
+        port:8080,
+        livereload: true
+    });
+});
+
+//sequence的返回函数只能运行一次 所以这里用function cb方式使用
+gulp.task('watchlist',function (cb) {
+    sequence('clean',['mincss','minimg','minjs','html'])(cb)
+});
+
+gulp.task('watch',function () {
+    gulp.watch(['./src/**'],['watchlist']);
+});
+
+
+//中括号外面的是串行执行， 中括号里面的任务是并行执行。
+gulp.task('default',function (cb) {
+    sequence('clean',['mincss','minimg','minjs','html','connect'],'watch')(cb)
 });
 
 
 
-
-gulp.task('default', function(){
-    gulp.run( "connect","watch","build-index","build-js-lib", "build-common-css","lint","img","jsmin","clean");
- // 监听html文件变化
-})
